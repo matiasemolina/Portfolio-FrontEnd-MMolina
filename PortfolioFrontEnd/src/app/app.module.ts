507a0222bc3ad1444b309ae14a41ca95c4f5e221
matiasemolina <matiasemolina1@gmail.com>
2023-04-30 23:11:22 -0300

    Commit Parcial Header.

--- PortfolioFrontEnd/src/app/app.module.ts
+++ PortfolioFrontEnd/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ArgProgramaComponent } from './components/arg-programa/arg-programa.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialsComponent } from './components/socials/socials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArgProgramaComponent,
+    HeaderComponent,
+    SocialsComponent
   ],
   imports: [
     BrowserModule,
---
+++ PortfolioFrontEnd/src/app/components/arg-programa/arg-programa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arg-programa',
+  templateUrl: './arg-programa.component.html',
+  styleUrls: ['./arg-programa.component.css']
+})
+export class ArgProgramaComponent {
+
+}
